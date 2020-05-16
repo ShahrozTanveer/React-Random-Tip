724d6b92939f5cb1a182c0c8433d9a034aa10ee6
SharozTanveer <saadtanveer7@live.co.uk>
2020-05-16 12:47:32 +0500

    changes ig

---
+++ build/precache-manifest.8334572ab90328da31fdb53df9c723a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "780e8dec49389b6271b2f910544e2f65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aad478c187e8feb191bc",
+    "url": "/static/css/main.ad38f024.chunk.css"
+  },
+  {
+    "revision": "72c2dce51aac20980b74",
+    "url": "/static/js/2.87c3b63f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.87c3b63f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aad478c187e8feb191bc",
+    "url": "/static/js/main.16c9497c.chunk.js"
+  },
+  {
+    "revision": "f468ad552bdcabf584e2",
+    "url": "/static/js/runtime-main.256676e7.js"
+  }
+]);
